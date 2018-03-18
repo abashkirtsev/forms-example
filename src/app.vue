aaad8a3000fbfaeecd42949ee151eb543fc407bf
Alexander Bashkirtsev <alexander.bashkirtsev@acronis.com>
2018-03-18 16:13:43 +0300

    Add loader

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <section class="todoapp">
+      <VLoader v-if="isLoading || isSaving" />
+
       <header class="header">
         <h1>todos</h1>
         <BlockNew />
@@ -17,11 +19,16 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 import BlockFooter from "./components/block-footer";
 import BlockList from "./components/block-list";
 import BlockNew from "./components/block-new";
+import VLoader from "./components/controls/loader";
 
 export default {
-  components: { BlockFooter, BlockList, BlockNew }
+  components: { BlockFooter, BlockList, BlockNew, VLoader },
+
+  computed: mapState("todos", ["isLoading", "isSaving"])
 };
 </script>
---
+++ src/components/controls/loader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="filler">
+    <div class="indicator" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.filler {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  opacity: 0.9;
+  background-color: white;
+  z-index: 5;
+}
+
+.indicator {
+  display: block;
+  width: 2rem;
+  height: 2rem;
+  border: 2px solid rgba(0, 0, 0, 0.1);
+  border-radius: 2rem;
+  border-left: 2px solid #5dc2af;
+  box-sizing: border-box;
+  animation: rotation 1s infinite linear;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
